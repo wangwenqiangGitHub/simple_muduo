c29424e873443d780bc37555eab6b2b021d8bfab
wangwenqiangGitHub <1103169969@qq.com>
2022-08-23 14:10:57 +0800

    [fix]InetAddress兼容ipv6

--- InetAddress.cpp
+++ InetAddress.cpp
@@ -1,44 +1,122 @@
-#include <cstring> // memset
-#include <string>
-
-#include <sys/socket.h>
+//=====================================================================
+//
+// InetAddress.cpp - 
+//
+// Created by wwq on 2022/08/22
+// Last Modified: 2022/08/22 17:33:44
+//
+//=====================================================================
 #include "InetAddress.h"
+#include "Logger.h"
+#include "SocketsOps.h"
+#include <assert.h>
+
+#include <cstring>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+static const in_addr_t kInaddrAny = INADDR_ANY;
+static const in_addr_t kInaddrLoopback = INADDR_LOOPBACK;
+#pragma GCC diagnostic error "-Wold-style-cast"
 
 using namespace simple_muduo;
 
+static_assert(sizeof(InetAddress) == sizeof(struct sockaddr_in6),
+              "InetAddress is same size as sockaddr_in6");
 
-InetAddress::InetAddress(uint64_t port,bool lookbackOnly, bool ipv6)
-{}
-InetAddress::InetAddress(std::string ip, uint64_t port)
+InetAddress::InetAddress(uint16_t portArg, bool loopbackOnly, bool ipv6)
 {
-    ::memset(&addr_, 0, sizeof(addr_));
+  if (ipv6)
+  {
+	::memset(&addr6_, 0, sizeof addr6_);
+    addr6_.sin6_family = AF_INET6;
+    in6_addr ip = loopbackOnly ? in6addr_loopback : in6addr_any;
+    addr6_.sin6_addr = ip;
+    addr6_.sin6_port = sockets::hostToNetwork16(portArg);
+  }
+  else
+  {
+	::memset(&addr_, 0, sizeof addr_);
     addr_.sin_family = AF_INET;
-    addr_.sin_port = ::htons(port); // 本地字节序转为网络字节序
-    addr_.sin_addr.s_addr = ::inet_addr(ip.c_str());
+    in_addr_t ip = loopbackOnly ? kInaddrLoopback : kInaddrAny;
+    addr_.sin_addr.s_addr = sockets::hostToNetwork32(ip);
+    addr_.sin_port = sockets::hostToNetwork16(portArg);
+  }
+}
+
+InetAddress::InetAddress(std::string ip, uint16_t portArg, bool ipv6)
+{
+  if (ipv6 || strchr(ip.c_str(), ':'))
+  {
+	::memset(&addr6_, 0, sizeof addr6_);
+    sockets::fromIpPort(ip.c_str(), portArg, &addr6_);
+  }
+  else
+  {
+	::memset(&addr_, 0, sizeof addr_);
+    sockets::fromIpPort(ip.c_str(), portArg, &addr_);
+  }
+}
+
+std::string InetAddress::toIpPort() const
+{
+  char buf[64] = "";
+  sockets::toIpPort(buf, sizeof buf, getSockAddr());
+  return buf;
 }
 
 std::string InetAddress::toIp() const
 {
-	// addr_
-	char buf[64] = {0};
-	//将点分十进制的ip地址转化为用于网络传输的数值格式
-	::inet_ntop(AF_INET,& addr_.sin_addr, buf, sizeof(buf));
-	return buf;
-
+  char buf[64] = "";
+  sockets::toIp(buf, sizeof buf, getSockAddr());
+  return buf;
 }
 
-std::string InetAddress:: toIpPort() const
+uint32_t InetAddress::ipv4NetEndian() const
 {
-    // ip:port
-    char buf[64] = {0};
-    ::inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof buf);
-    size_t end = ::strlen(buf);
-    uint16_t port = ::ntohs(addr_.sin_port);
-    sprintf(buf+end, ":%u", port);
-    return buf;
+  assert(family() == AF_INET);
+  return addr_.sin_addr.s_addr;
 }
 
-uint16_t InetAddress::toPort() const
+uint16_t InetAddress::port() const
 {
-	return ::ntohs(addr_.sin_port);
+  return sockets::networkToHost16(portNetEndian());
+}
+
+static __thread char t_resolveBuffer[64 * 1024];
+
+bool InetAddress::resolve(std::string hostname, InetAddress* out)
+{
+  assert(out != NULL);
+  struct hostent hent;
+  struct hostent* he = NULL;
+  int herrno = 0;
+  ::memset(&hent, 0, sizeof(hent));
+
+  int ret = gethostbyname_r(hostname.c_str(), &hent, t_resolveBuffer, sizeof t_resolveBuffer, &he, &herrno);
+  if (ret == 0 && he != NULL)
+  {
+    assert(he->h_addrtype == AF_INET && he->h_length == sizeof(uint32_t));
+    out->addr_.sin_addr = *reinterpret_cast<struct in_addr*>(he->h_addr);
+    return true;
+  }
+  else
+  {
+    if (ret)
+    {
+	  LOG_INFO("InetAddress::resolve\n");
+    }
+    return false;
+  }
+}
+
+void InetAddress::setScopeId(uint32_t scope_id)
+{
+  if (family() == AF_INET6)
+  {
+    addr6_.sin6_scope_id = scope_id;
+  }
 }
--- InetAddress.h
+++ InetAddress.h
@@ -1,43 +1,68 @@
-#ifndef SIMPLE_MUDUO_H
-#define SIMPLE_MUDUO_H
+//=====================================================================
+//
+// InetAddress.h - 
+//
+// Created by wwq on 2022/08/22
+// Last Modified: 2022/08/22 17:26:49
+//
+//=====================================================================
+#ifndef SIMPLE_MUDUO_INETADDRESS_H
+#define SIMPLE_MUDUO_INETADDRESS_H
+#include "noncopyable.h"
 
-#include <arpa/inet.h>
-#include <cstdint>
 #include <netinet/in.h>
 #include <string>
 
-#include "SocketsOps.h"
-namespace simple_muduo {
+namespace simple_muduo
+{
+namespace sockets
+{
+const struct sockaddr* sockaddr_cast(const struct sockaddr_in6* addr);
+}
+
+///
+/// Wrapper of sockaddr_in.
+///
+/// This is an POD interface class.
 class InetAddress
 {
-public:
-	//TODO: ipv6
-	explicit InetAddress(uint64_t port = 0,bool lookbackOnly = false, bool ipv6 = false);
-	explicit InetAddress(std::string ip, uint64_t port);
-    explicit InetAddress(const sockaddr_in &addr)
-        : addr_(addr)
-    { }
-	explicit InetAddress(const struct sockaddr_in6& addr)
-		: addr6_(addr)
-	{ }
+ public:
+  explicit InetAddress(uint16_t port = 0, bool loopbackOnly = false, bool ipv6 = false);
+  InetAddress(std::string ip, uint16_t port, bool ipv6 = false);
 
-    std::string toIp() const;
-    std::string toIpPort() const;
-    uint16_t toPort() const;
+  explicit InetAddress(const struct sockaddr_in& addr)
+    : addr_(addr)
+  { }
 
-	sa_family_t family() const { return addr_.sin_family; }
-    // const sockaddr_in *getSockAddr() const { return &addr_; }
-	// const struct sockaddr* getSockAddr() const { return sockets::sockaddr_cast(&addr6_); }
-	const struct sockaddr* getSockAddr() const { return sockets::sockaddr_cast(&addr_); }
-    void setSockAddr(const sockaddr_in &addr) { addr_ = addr; }
-	void setSockAddrInet6(const struct sockaddr_in6& addr6) { addr6_ = addr6; }
+  explicit InetAddress(const struct sockaddr_in6& addr)
+    : addr6_(addr)
+  { }
 
-private:
-	union
-	{
-		struct sockaddr_in addr_;
-		struct sockaddr_in6 addr6_;
-	};
+  sa_family_t family() const { return addr_.sin_family; }
+  std::string toIp() const;
+  std::string toIpPort() const;
+  uint16_t port() const;
+
+  const struct sockaddr* getSockAddr() const { return sockets::sockaddr_cast(&addr6_); }
+  void setSockAddrInet6(const struct sockaddr_in6& addr6) { addr6_ = addr6; }
+
+  uint32_t ipv4NetEndian() const;
+  uint16_t portNetEndian() const { return addr_.sin_port; }
+
+  static bool resolve(std::string hostname, InetAddress* result);
+  // static std::vector<InetAddress> resolveAll(const char* hostname, uint16_t port = 0);
+
+  // set IPv6 ScopeID
+  void setScopeId(uint32_t scope_id);
+
+ private:
+  union
+  {
+    struct sockaddr_in addr_;
+    struct sockaddr_in6 addr6_;
+  };
 };
-}
-#endif
+
+}  // namespace simple_muduo
+
+#endif  // MUDUO_NET_INETADDRESS_H
